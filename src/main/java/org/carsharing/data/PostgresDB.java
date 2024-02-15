3859e29c82473c104674ebe8e4c9e278fbf47d15
Friday <230040@astanait.edu.kz>
2024-02-15 10:27:00 +0600

    postgre update

--- src/main/java/org/carsharing/data/PostgresDB.java
+++ src/main/java/org/carsharing/data/PostgresDB.java
@@ -6,10 +6,10 @@ import java.sql.*;
 public class PostgresDB implements IDB {
     @Override
     public Connection getConnection() {
-        String connectionUrl = "jdbc:postgresql://localhost:5432/ASSING3";
+        String connectionUrl = "jdbc:postgresql://localhost:5432/examokedb";
         try {
             Class.forName("org.postgresql.Driver");
-            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "1234");
+            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "1973");
             return con;
         } catch (Exception e) {
             System.out.println("failed to connect to postgres: " + e.getMessage());
